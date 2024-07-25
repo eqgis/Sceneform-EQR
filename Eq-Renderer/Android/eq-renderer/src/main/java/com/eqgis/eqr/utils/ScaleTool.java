c40ab54ae9f0d7763b4a528831f37b512b06e5a9
Ikkyu_tanyx_eqgis <a451341812@qq.com>
2024-07-25 20:11:41 +0800

    添加计算成单位模型尺寸的方法

---
+++ Eq-Renderer/Android/eq-renderer/src/main/java/com/eqgis/eqr/utils/ScaleTool.java
@@ -0,0 +1,37 @@
+package com.eqgis.eqr.utils;
+
+import com.google.ar.sceneform.collision.Box;
+import com.google.ar.sceneform.collision.CollisionShape;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Renderable;
+
+/**
+ * 缩放比例工具类
+ * @author tanyx 2024/7/25
+ * @version 1.0
+ * <br/>SampleCode:<br/>
+ * <code>
+ *
+ * </code>
+ **/
+public class ScaleTool {
+
+    /**
+     * 计算缩放成单位尺寸（最长边为1m）所用的比例
+     * <p>需要在Node.setRenderable(modelRenderable);后使用才有效</p>
+     * @param renderable 渲染对象。
+     * @return
+     */
+    public static float calculateUnitsScale(Renderable renderable){
+        CollisionShape collisionShape = renderable.getCollisionShape();
+
+        if (collisionShape instanceof Box){
+            Box box = (Box) collisionShape;
+            Vector3 size = box.getSize();
+            float max = Math.max(size.x,Math.max(size.y,size.z));
+            return 1.0f / max;
+        }
+
+        return 1.0f;
+    }
+}
--- SampleProj/app/src/main/java/com/eqgis/test/ARSceneActivity.java
+++ SampleProj/app/src/main/java/com/eqgis/test/ARSceneActivity.java
@@ -1,10 +1,14 @@
 package com.eqgis.test;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 import com.eqgis.ar.ARCamera;
 import com.eqgis.ar.ARPlugin;
+import com.eqgis.eqr.gesture.NodeGestureController;
 import com.eqgis.test.scene.GltfSampleScene;
 import com.google.ar.sceneform.ARPlatForm;
 import com.huawei.hiar.AREnginesApk;
@@ -21,8 +25,24 @@ public class ARSceneActivity extends BaseActivity {
         setContentView(R.layout.activity_ar_scene);
 
         sceneLayout = findViewById(R.id.ar_scene_layout);
+        //节点手势控制器初始化
+        NodeGestureController.getInstance()
+                .setCamera(sceneLayout.getCamera())
+                .init(this)
+                .setEnabled(true);
+
         //加载GLTF模型
         sampleScene = new GltfSampleScene();
-        sampleScene.create(this,sceneLayout.getRootNode());
+        sampleScene.create(getApplicationContext(),sceneLayout.getRootNode());
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        View touchView = findViewById(R.id.touch_view);
+        touchView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                NodeGestureController.getInstance().onTouch(motionEvent);
+                return true;
+            }
+        });
     }
 }
