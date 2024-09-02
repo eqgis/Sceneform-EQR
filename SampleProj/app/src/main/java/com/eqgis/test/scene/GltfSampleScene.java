95a6cef2cb4d66a1f367c28a203692e5101d4f57
Ikkyu_tanyx_eqgis <a451341812@qq.com>
2024-09-02 19:32:50 +0800

    模型动画示例

--- SampleProj/app/src/main/java/com/eqgis/test/scene/GltfSampleScene.java
+++ SampleProj/app/src/main/java/com/eqgis/test/scene/GltfSampleScene.java
@@ -5,6 +5,9 @@ import android.net.Uri;
 import android.view.MotionEvent;
 import android.widget.Toast;
 
+import com.eqgis.eqr.animation.ARAnimationModel;
+import com.eqgis.eqr.animation.ARAnimationParameter;
+import com.eqgis.eqr.animation.ARAnimationRepeatMode;
 import com.eqgis.eqr.gesture.NodeGestureController;
 import com.eqgis.eqr.node.RootNode;
 import com.eqgis.eqr.utils.PoseUtils;
@@ -27,7 +30,6 @@ import java.util.function.Function;
 public class GltfSampleScene implements ISampleScene{
     private String modelPath = "gltf/bee.glb";
     public float distance = 3.6f;
-
 
     /**
      * 模型节点
@@ -77,9 +79,11 @@ public class GltfSampleScene implements ISampleScene{
                         //缩放成单位尺寸
                         modelNode.setLocalScale(Vector3.one()
                                 .scaled(ScaleTool.calculateUnitsScale(modelRenderable)));
-//                        modelNode.setLocalRotation(new Quaternion(Vector3.up(),30));
                         modelNode.setLocalPosition(new Vector3(0f, 0, -distance));
                         modelNode.setParent(rootNode);
+
+                        //创建模型动画
+                        createAnimation(modelNode);
                         return null;
                     }
                 });
@@ -121,5 +125,28 @@ public class GltfSampleScene implements ISampleScene{
                 .setShadowCastingEnabled(/*启用光线投射，这会产生阴影，默认未启用*/true)
                 .build();
         lightNode.setLight(light);
+    }
+
+
+    /**
+     * */
+    public void createAnimation(Node node){
+        if (node.getRenderableInstance() == null) {
+            return;
+        }
+        int animationCount = node.getRenderableInstance().getAnimationCount();
+        if (animationCount > 0){
+            //创建动画参数（这里主要设置周期）
+            ARAnimationParameter parameter = new ARAnimationParameter();
+            parameter.setDuration(6000L);//设置播放周期
+            parameter.setRepeatMode(ARAnimationRepeatMode.INFINITE);//设置循环方式
+            //创建默认动画
+            ARAnimationModel animationModel = new ARAnimationModel(node);
+            animationModel.createAnimation(parameter);
+            //默认播放第一个索引的动画
+            animationModel.setCurrentIndex(0);
+            //播放动画
+            animationModel.play();
+        }
     }
 }
