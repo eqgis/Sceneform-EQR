ae480c09b6ab1126d8f418e1b883e05ea4080133
Ikkyu_tanyx_eqgis <a451341812@qq.com>
2024-10-11 10:35:25 +0800

    Update CameraActivity2.java

--- SampleProj/app/src/main/java/com/eqgis/test/CameraActivity2.java
+++ SampleProj/app/src/main/java/com/eqgis/test/CameraActivity2.java
@@ -46,6 +46,7 @@ public class CameraActivity2 extends BaseActivity {
         //加载GLTF模型
         sampleScene = new GltfSampleScene();
         sampleScene.create(getApplicationContext(),sceneLayout.getRootNode());
+        sampleScene.setSceneView(sceneLayout.getSceneView());
 
         @SuppressLint({"MissingInflatedId", "LocalSuppress"})
         View touchView = findViewById(R.id.touch_view);
