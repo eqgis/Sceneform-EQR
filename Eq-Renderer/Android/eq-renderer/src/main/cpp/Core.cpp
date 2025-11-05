253326f09cef7d7b77368d793faae2cacce72e79
Ikkyu_tanyx_eqgis <52307942+eqgis@users.noreply.github.com>
2025-11-05 20:20:13 +0800

    Update version

--- Eq-Renderer/Android/eq-renderer/src/main/cpp/Core.cpp
+++ Eq-Renderer/Android/eq-renderer/src/main/cpp/Core.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 //版本信息
-string EQR_CORE_VERSION = "EQ-Renderer_v1.0.9";
+string EQR_CORE_VERSION = "EQ-Renderer_v1.1.0";
 string FILAMENT_VERSION = "v1.53.4";
 
 //更新核心模块状态值
